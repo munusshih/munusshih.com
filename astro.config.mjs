b88c3cf328a5e7ec1d98c61ef4d82a5a36099b97
Munus Shih <munusshih@newschool.edu>
2025-05-24 03:59:52 -0400

    SEO + A11y

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://munusshih-com.vercel.app/",
+  site: "https://munusshih.com",
   output: "static",
   adapter: vercel({
     webAnalytics: {
